fd8830fbc1152ffe91950e00986f58d8f6a1e853
Caelin Jackson-King <caelink@gmail.com>
2018-07-15 22:22:39 -0700

    Initial Commit

---
+++ mtg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mtg
+//
+//  Created by Caelin Jackson-King on 2018-07-15.
+//  Copyright © 2018 Caelin Jackson-King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
